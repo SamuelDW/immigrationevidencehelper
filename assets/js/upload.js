bc91a268f5b047d77e396d7dad21b2d3908151a4
Samuel DW <s.durrantwalker@yordasgroup.com>
2021-11-13 21:36:00 +0000

    currently works up to creating the array

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,16 +1,42 @@
-let skypeConversation;
+/**
+ * Declaring Objects
+ */
+let callInfo = {
+    whoStartedCall: false,
+    participentOne: false,
+    participentTwo: false,
+    duration: false,
+    timeStarted: false,
+    timeEnded: false,
+    callId: false,
+    callDate: false,
+    callType: false,
+}
+//DURATION IS IN SECONDS
+
 window.addEventListener('load', function () {
     document.getElementById('tar-upload-input').addEventListener('change',function (e) {
 
         let reader = new FileReader();
         reader.onload = function(event) {
             let skypeJsonObject = JSON.parse(event.target.result),
-                skypePrimaryUser = skypeJsonObject.userId
                 skypeConversation = skypeJsonObject.conversations
 
+            // Get all the events that are calls, discard the rest
+            // And then create the information from this object
+            console.time('test')
             const callEvents = getAllCallEvents(skypeConversation)
+            let callInformationArray = []
+            callEvents.forEach(call => {
+                let callObject = createCallInformationObject(call, callInfo)
+                if (callObject) {
+                    console.log(callObject)
+                    callInformationArray.push(callObject)
+                }
+            })
+            console.log(callInformationArray)
+            console.timeEnd('test')
         }
-
         reader.readAsText(e.target.files[0]);
     })
 })
@@ -28,30 +54,65 @@ function getAllCallEvents(conversations) {
             }
         })
     })
+
     return callEvents
 }
 
 /**
- * Splits the json into call events and messages
- * @param {Array} conversations
+ * Creates the call information object from the Skype Import data
+ * @param {Object} call
+ * @param {Object} CallInformationObject
+ * @returns CallInformationObject with filled data
  */
-function splitCalls(conversations) {
-    conversations.forEach(conversation => {
-        conversation.MessageList.forEach(message => {
-            if (message.messagetype === 'Event/Call') {
-                const xmlStr = message.content,
-                    parser = new DOMParser(),
-                    doc = parser.parseFromString(xmlStr, "application/xml"),
-                    errorNode = doc.querySelector("parseerror")
+function createCallInformationObject(call, CallInformationObject) {
+    let parser = new DOMParser(),
+        callInformation = parser.parseFromString(call.content, 'text/xml'),
+        callParticipents = callInformation.getElementsByTagName('name'),
+        callIdentifier = callInformation.getElementsByTagName('partlist')[0],
+        callType = callIdentifier.attributes['type'],
+        callDate = new Date(call.originalarrivaltime),
+        callEventId = callIdentifier.attributes['callId'],
+        callDurationArray = callInformation.getElementsByTagName('duration'),
+        callDuration = null
 
-                    //console.log(xmlStr)
+    if (!callType) {
+        return
+    }
+    if (!callEventId) {
+        return
+    }
+    if (callDurationArray.length !== 0) {
+        callDuration = callDurationArray[0].innerHTML
+    }
 
-                    if (errorNode) {
-                        console.log("error while parsing")
-                    } else {
-                        //console.log(doc.documentElement)
-                    }
-            }
-        })
-    })
+    CallInformationObject.callType = callType.value
+    CallInformationObject.callId = callEventId.value
+    CallInformationObject.whoStartedCall = call.displayName
+    CallInformationObject.callDate = callDate
+    CallInformationObject.callType = callType.value
+    CallInformationObject.duration = callDuration
+
+    callParticipents = getCallParticipents(callParticipents)
+    CallInformationObject.participentOne = callParticipents[0]
+    CallInformationObject.participentTwo = callParticipents[1]
+
+    if (!CallInformationObject.whoStartedCall) {
+        CallInformationObject.whoStartedCall = CallInformationObject.participentOne
+    }
+
+    return CallInformationObject
+}
+
+/**
+ *
+ * @param {Array} callGuests
+ * @returns array of the two guests in the call
+ */
+function getCallParticipents(callGuests) {
+    let callParticipents = [
+        callGuests[0].innerHTML,
+        callGuests[1].innerHTML,
+    ]
+
+    return callParticipents
 }
