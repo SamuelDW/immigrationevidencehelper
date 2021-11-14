5933034ac2e4a452c8861bc151431234fa75fc54
Samuel DW <s.durrantwalker@yordasgroup.com>
2021-11-14 21:42:41 +0000

    table is now viewable

--- assets/js/display_results.js
+++ assets/js/display_results.js
@@ -1,10 +1,12 @@
-function displayResultsAsTable(callArray) {
-
+function displayResultsAsTable(callArray, table) {
+    let tableBody = document.getElementById(table).getElementsByTagName('tbody')[0]
     callArray.forEach(call => {
+        let row = '<tr><td>' + call.date + '</td>'
+            + '<td>' + call.duration + '</td>'
+            + '<td>' + call.participentOne + ', ' + call.participentTwo + '</td>'
+            + '<td>' + call.whoStartedCall + '</td></tr>'
 
+        let newRow = tableBody.insertRow(tableBody.rows.length)
+        newRow.innerHTML = row
     })
-}
-
-function addHeaders() {
-
 }
